6a4d599443da58a2a0c24893f466bec0cf775860
serknight <chrisknight.mail@gmail.com>
2020-04-20 16:36:07 -0600

    sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_04_08_220514) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
