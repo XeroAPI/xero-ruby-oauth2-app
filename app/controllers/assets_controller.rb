39f83c15577b00000ef2827d705637b0cf3608cb
serknight <chrisknight.mail@gmail.com>
2020-04-24 12:56:43 -0600

    assets working

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -10,6 +10,7 @@ class AssetsController < ActionController::Base
       "assetStatus": "DRAFT"
     }
     @asset = xero_client.asset_api.create_asset(current_user.active_tenant_id, asset)
+    @assets = xero_client.asset_api.get_assets(current_user.active_tenant_id, 'DRAFT').items # 'REGISTERED'
   end
 
 end
