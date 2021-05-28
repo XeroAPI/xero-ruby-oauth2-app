d71679649b711cba475aad104caf7313bd4f568b
serknight <chrisknight.mail@gmail.com>
2021-05-28 12:45:33 -0600

    add params code check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,18 @@ class ApplicationController < ActionController::Base
   end
 
   def callback
-    @token_set = @xero_client.get_token_set_from_callback(params)
-    # you can use `@xero_client.connections` to fetch info about which orgs
-    # the user has authorized and the most recently connected tenant_id
-    current_user.token_set = @token_set if !@token_set["error"]
-    current_user.token_set['connections'] = @xero_client.connections
-    current_user.active_tenant_id = latest_connection(current_user.token_set['connections'])
-    current_user.save!
-    flash.notice = "Successfully received Xero Token Set"
+    if params['code']
+      @token_set = @xero_client.get_token_set_from_callback(params)
+      # you can use `@xero_client.connections` to fetch info about which orgs
+      # the user has authorized and the most recently connected tenant_id
+      current_user.token_set = @token_set if !@token_set["error"]
+      current_user.token_set['connections'] = @xero_client.connections
+      current_user.active_tenant_id = latest_connection(current_user.token_set['connections'])
+      current_user.save!
+      flash.notice = "Successfully received Xero Token Set"
+    else
+      flash.notice = "Failed to received Xero Token Set"
+    end
   end
 
   def refresh_token
